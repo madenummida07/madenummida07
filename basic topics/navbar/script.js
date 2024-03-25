ffd4eeaa70bfb5a0894521e054b829ed0010bfa5
Ummida Maden <abdurahim.amirovv@gmail.com>
2024-03-25 20:46:56 +0600

    added pages

---
+++ basic topics/navbar/script.js
@@ -0,0 +1,6 @@
+const sidebar = document.querySelector(".sidebar");
+const navLink = document.ATTRIBUTE_NODE.querySelector(".nav_link");
+
+sidebar.addEventListener("click", () => {
+  navLink.classList.toggle("hide");
+});
