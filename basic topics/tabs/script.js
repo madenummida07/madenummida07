3bb8a7c593f0ceb139ce50c7114df4a30e0ea3e8
Ummida Maden <abdurahim.amirovv@gmail.com>
2024-04-05 10:15:43 +0600

    added

--- basic topics/tabs/script.js
+++ basic topics/tabs/script.js
@@ -41,6 +41,54 @@
 
 // showPanel(0, "#f44336");
 
+// var tabButtons = document.querySelectorAll(
+//   ".tabContainer .buttonContainer button"
+// );
+
+// var tabPanels = document.querySelectorAll(".tabContainer .tabPanel");
+
+// function showPanel(panelIndex, colorCode) {
+//   tabButtons.forEach(function (node) {
+//     node.style.backgroundColor = "";
+//     node.style.color = "";
+//   });
+
+//   tabButtons[panelIndex].style.backgroundColor = colorCode;
+//   tabButtons[panelIndex].style.color = "white";
+//   tabPanels.forEach(function (node) {
+//     node.style.display = "none";
+//   });
+
+//   tabPanels[panelIndex].style.display = "block";
+//   tabPanels[panelIndex].style.backgroundColor = colorCode;
+// }
+
+// showPanel(0, "#f44336");
+
+// var tabButtons = document.querySelectorAll(
+//   ".tabContainer .buttonContainer button"
+// );
+
+// var tabPanels = document.querySelectorAll(".tabContainer .tabPanel");
+
+// function showPanel(panelIndex, colorCode) {
+//   tabButtons.forEach(function (node) {
+//     node.style.backgroundColor = "";
+//     node.style.color = "";
+//   });
+
+//   tabButtons[panelIndex].style.backgroundColor = colorCode;
+//   tabButtons[panelIndex].style.color = "white";
+//   tabPanels.forEach(function (node) {
+//     node.style.display = "none";
+//   });
+
+//   tabPanels[panelIndex].style.display = "block";
+//   tabPanels[panelIndex].style.backgroundColor = colorCode;
+// }
+
+// showPanel(0, "#f44336");
+
 var tabButtons = document.querySelectorAll(
   ".tabContainer .buttonContainer button"
 );
@@ -59,8 +107,8 @@ function showPanel(panelIndex, colorCode) {
     node.style.display = "none";
   });
 
-  tabPanels[panelIndex].style.display = "block";
   tabPanels[panelIndex].style.backgroundColor = colorCode;
+  tabPanels[panelIndex].style.display = "block";
 }
 
 showPanel(0, "#f44336");
---
+++ quizzes/quiz7.js
@@ -0,0 +1,5 @@
+function towerHanoi(num) {
+  let move = Math.pow(2, num) - 1;
+  console.log(move);
+}
+towerHanoi();
